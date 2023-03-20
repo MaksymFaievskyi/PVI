25c052ac69629ffd76117f8c79134edb4694ef47
Maksym Faevskyi <maksim.faevskyi@gmail.com>
2023-03-20 21:11:31 +0200

    Create PWA

---
+++ Lab1/sw.js
@@ -0,0 +1,56 @@
+const staticCacheName = 's-app-v1';
+const dynamicCacheName = 'd-app-v1';
+
+const assetUrls = [
+  'student.html',
+  '/js/student.js',
+  '/css/student.css',
+  'offline.html'
+];
+
+self.addEventListener('install', async event => {
+  console.log("success install");
+  const cache = await caches.open(staticCacheName);
+  await cache.addAll(assetUrls);
+})
+
+self.addEventListener('activate', async event => {
+    console.log("success activate");
+  const cacheNames = await caches.keys()
+  await Promise.all(
+    cacheNames
+      .filter(name => name !== staticCacheName)
+      .filter(name => name !== dynamicCacheName)
+      .map(name => caches.delete(name))
+  )
+})
+
+self.addEventListener('fetch', event => {
+  console.log("success fetch");
+  const {request} = event;
+
+  const url = new URL(request.url);
+  if (url.origin === location.origin) {
+    event.respondWith(cacheFirst(request));
+  } else {
+    event.respondWith(networkFirst(request));
+  }
+})
+
+
+async function cacheFirst(request) {
+  const cached = await caches.match(request);
+  return cached ?? await fetch(request);
+}
+
+async function networkFirst(request) {
+  const cache = await caches.open(dynamicCacheName)
+  try {
+    const response = await fetch(request);
+    await cache.put(request, response.clone());
+    return response;
+  } catch (e) {
+    const cached = await cache.match(request);
+    return cached ?? await caches.match('/offline.html');
+  }
+}
